5228c8b5fb2161fd3ec6f8fe49b110b3079ac146
sazsu <149969207+sazsu@users.noreply.github.com>
2026-01-11 23:39:03 +0300

    fix(): return UserId instead of UUID

--- src/health_backend/infra/common/identity_provider/mock.py
+++ src/health_backend/infra/common/identity_provider/mock.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 from uuid import UUID
 
 from health_backend.application.common.identity_provider import IdentityProvider
-from health_backend.domain.user.entity import User
+from health_backend.domain.user.entity import User, UserId
 
 
 @dataclass
 class MockIdentityProvider(IdentityProvider):
     user: User
 
-    async def get_current_user_id(self) -> UUID:
+    async def get_current_user_id(self) -> UserId:
         return self.user.id
