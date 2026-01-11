4a2319de1da49ee20d78bb966a9092cebb60f493
sazsu <149969207+sazsu@users.noreply.github.com>
2026-01-11 23:36:56 +0300

    feat(): remove admin and doctor entities

--- src/health_backend/domain/doctor/repository.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-from health_backend.domain.doctor.entity import Doctor
-
-
-class DoctorRepository(ABC):
-    @abstractmethod
-    async def add(self, doctor: Doctor) -> None: ...
