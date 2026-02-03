4b775159f4dd8ce91c8c84dbe94ecc6797dcd5a0
sazsu <149969207+sazsu@users.noreply.github.com>
2026-02-03 22:10:13 +0300

    chore(dev): add a devshell with docker

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    with flake-utils.lib;
+      eachSystem defaultSystems (
+        system: let
+          pkgs = import nixpkgs {inherit system;};
+        in {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              colima
+              docker
+              docker-compose
+            ];
+          };
+        }
+      );
+}
