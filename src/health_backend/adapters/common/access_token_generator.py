2fe78be1ffe51529ed8ccf8ba3d01b80eef9c8b7
sazsu <149969207+sazsu@users.noreply.github.com>
2026-02-07 14:36:59 +0300

    feat(): add access token generator and its implementation

---
+++ src/health_backend/adapters/common/access_token_generator.py
@@ -0,0 +1,29 @@
+import time
+from dataclasses import dataclass
+
+import jwt
+
+from health_backend.application.common.access_token_generator import (
+    AccessToken,
+    AccessTokenGenerator,
+)
+from health_backend.domain.doctor.entity import DoctorId
+
+algorithm = 'HS256'
+
+
+@dataclass(frozen=True, slots=True)
+class JWTGenerator(AccessTokenGenerator):
+    secret: str
+
+    def generate(self, doctor_id: DoctorId, expire_in: int) -> AccessToken:
+        now = int(time.time())
+        token = jwt.encode(
+            payload={
+                'sub': str(doctor_id),
+                'exp': now + expire_in,
+                'iat': now,
+            },
+            key=self.secret,
+        )
+        return AccessToken(token)
---
+++ src/health_backend/application/common/access_token_generator.py
@@ -0,0 +1,15 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Protocol
+
+from health_backend.domain.doctor.entity import DoctorId
+
+
+@dataclass(frozen=True, slots=True)
+class AccessToken:
+    value: str
+
+
+class AccessTokenGenerator(Protocol):
+    @abstractmethod
+    def generate(self, doctor_id: DoctorId, expire_in: int) -> AccessToken: ...
